3ecd111023224fc57e6fab5ec32f736855864e82
eliftabanca <elifbusratabanca@gmail.com>
2024-02-19 15:20:53 +0300

    Txt dosyasından isim çekme eklendi.

--- pairs_names.py
+++ pairs_names.py
@@ -1,19 +1,25 @@
 #This bot makes grouping easier for pairs in the lesson.
 #It puts 5 different people for each pair, creating a total of 6 pairs.
 import random
-def get_names():
-    return ['Abdullah KESKIN', 'Ahmet Suat Tanıs', 'Anıl Aytac Ayyıldız','Asiye Kevser Yılmaz','Dilara Jiyan Eser',
-              'Elif Busra Tabanca', 'Emine Çoskun','Emir YILMAZ', 'Gül Karamahmutoglu', 'Gulen Yesilyurt', 'Halit Eren',
-              'Hanife Cennet ALKAN','Hulya Gökdemir', 'Mehmet Turgut Aksel', 'Merve Koyunyerli', 'Meryem Ozgun',
-              'Muhsine TASCI', 'Nesibe Kaya', 'Oguzhan Selmi', 'Onur Bülgü' 'Özlem Akman', 'Recep Kızıl', 'Rumeysa Coskun',
-              'Salim İla','Sevda Yılmaz', 'Süleyman Kulaksız','Tuba Bezek', 'Yasemin Karasakal Coban', 'Zehra Senturk',
-              'Zekine Zeliha Koc Demirci']
 
 
+
+import random
+file_path = "class_members_list.txt"
+
+def get_names_from_file(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        names = file.read().splitlines()
+    return names
+
+
+names = get_names_from_file(file_path)
 x = 1
-for i in range(6):
-    pairs = random.sample(get_names(), 5)
-    print(f"{x}. Pair: {','.join(pairs)} ")
+for _ in range(6):
+    
+    pairs = random.sample(names, 5)
+    print(f"{x}. Pair: {', '.join(pairs)}")
     x += 1
-    for people in pairs:
-        get_names().remove(people)
+    for person in pairs:
+        names.remove(person)
+
